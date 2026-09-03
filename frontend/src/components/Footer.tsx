5ef26d47d10b86dffda32855eab6bba3982137b0
duyhuy25 <duyhuy1342005@gmail.com>
2026-09-03 21:52:06 +0700

    update phần cuối trang

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Search, Heart, ShieldCheck, Mail, Phone, MapPin } from 'lucide-react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer style={{
+      height: '100px'
+    }}>
+      hello
+    </footer>
+  );
+};
